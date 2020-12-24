a1712aca220e9c4c7f0a7c28c08b2d1c25d585a3
alex-dr1 <alex.driagunov@yandex.ru>
2020-12-24 18:55:42 +0500

    don't work rest

--- src/main/java/ru/alex/testwork/Application.java
+++ src/main/java/ru/alex/testwork/Application.java
@@ -2,7 +2,6 @@ package ru.alex.testwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/ru/alex/testwork/camelroutes/MyRoute.java
+++ src/main/java/ru/alex/testwork/camelroutes/MyRoute.java
@@ -1,6 +1,7 @@
 package ru.alex.testwork.camelroutes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
 
@@ -9,13 +10,29 @@ public class MyRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-//        from("timer:timer?period=1000").routeId("hello")
-//                .to("log:out>>>>>>>>>>>>>?showAll=true")
-                from("file:outbox?noop=true")
-                .to("log:file>>")
-                .convertBodyTo(String.class)
-                        .to("log:file>>")
-                .setBody().xpath("//bean/camelcontext/@id", String.class)
-                .to("log:xml>>");
+//        from("timer:timer?period=1000&repeatCount=1")
+//                .to("log:log1?showAll=true")
+        restConfiguration().component("servlet")
+                .port("8080")
+                .bindingMode(RestBindingMode.json)
+        ;
+
+        rest("/api")
+                .get("/run")
+                .outType(String.class)
+                .responseMessage()
+                .code(204)
+                .message("Running...")
+                .endResponseMessage()
+                .to("log:log2?showAll=true")
+                .to("direct:start")
+        ;
+
+//        from("direct:start")
+//                .pollEnrich("file:inbox?noop=true")
+//                .to("log:log2?showAll=true")
+//                .filter(header("CamelFileName").endsWith(".xml"))
+//                .to("log:log3?showAll=true")
+//        ;
     }
 }
